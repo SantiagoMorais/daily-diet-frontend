94ff0512d3616e51107ff2a0fdd4d8eea4480373
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-11-07 18:47:34 -0300

    refactor(WelcomeBanner): separing the elements by files

---
+++ src/components/ui/welcomeBanner/profileButton/accordion/index.tsx
@@ -0,0 +1,38 @@
+import { LogoutButton } from "@components/ui/welcomeBanner/logoutButton";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Collapse } from "react-collapse";
+import { Link } from "react-router-dom";
+import { welcomeBannerButtons } from "utils/arrays";
+import { afterHover } from "@styles/index";
+
+export const Accordion = ({ windowOppened }: { windowOppened: boolean }) => {
+  return (
+    <div
+      className={`absolute z-20 mt-3 flex w-full flex-col overflow-hidden rounded-3xl bg-white p-0 duration-500 ${windowOppened && "p-2"}`}
+    >
+      <Collapse isOpened={windowOppened}>
+        {welcomeBannerButtons.map((button) => (
+          <Link
+            key={button.id}
+            to={button.link}
+            className="group relative mb-2 block w-full overflow-hidden rounded-full py-2 text-base text-bg opacity-80 duration-500"
+          >
+            <span className={afterHover()}></span>
+            <p className="relative z-10 px-3 duration-500 group-hover:text-white">
+              {button.title}
+            </p>
+          </Link>
+        ))}
+        <hr className="border-zinc-300" />
+        <LogoutButton>
+          <span className={afterHover()}></span>
+          <p className="z-10 flex w-full items-center justify-between px-3">
+            Logout
+            <FontAwesomeIcon icon={faRightFromBracket} className="text-lg" />
+          </p>
+        </LogoutButton>
+      </Collapse>
+    </div>
+  );
+};
--- src/components/ui/welcomeBanner/profileButton/index.tsx
+++ src/components/ui/welcomeBanner/profileButton/index.tsx
@@ -1,15 +1,8 @@
-import { LogoutButton } from "@components/ui/logoutButton";
-import {
-  faCircleUser,
-  faRightFromBracket,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IProfileDataReturn } from "@functions/handleProfileData";
 import { useState } from "react";
-import { Collapse } from "react-collapse";
-import { Link } from "react-router-dom";
-import { welcomeBannerButtons } from "utils/arrays";
-import { afterHover } from "@styles/index";
+import { Accordion } from "./accordion";
 
 export const ProfileButton = (data: IProfileDataReturn) => {
   const [windowOppened, setWindowOppened] = useState<boolean>(false);
@@ -27,32 +20,7 @@ export const ProfileButton = (data: IProfileDataReturn) => {
         </h2>
       </button>
 
-      <div
-        className={`absolute z-20 mt-3 flex w-full flex-col overflow-hidden rounded-3xl bg-white p-0 duration-500 ${windowOppened && "p-2"}`}
-      >
-        <Collapse isOpened={windowOppened}>
-          {welcomeBannerButtons.map((button) => (
-            <Link
-              key={button.id}
-              to={button.link}
-              className="group relative mb-2 block w-full overflow-hidden rounded-full py-2 text-base text-bg opacity-80 duration-500"
-            >
-              <span className={afterHover()}></span>
-              <p className="relative z-10 px-3 duration-500 group-hover:text-white">
-                {button.title}
-              </p>
-            </Link>
-          ))}
-          <hr className="border-zinc-300" />
-          <LogoutButton>
-            <span className={afterHover()}></span>
-            <p className="z-10 flex w-full items-center justify-between px-3">
-              Logout
-              <FontAwesomeIcon icon={faRightFromBracket} className="text-lg" />
-            </p>
-          </LogoutButton>
-        </Collapse>
-      </div>
+      <Accordion windowOppened={windowOppened} />
     </div>
   );
 };
