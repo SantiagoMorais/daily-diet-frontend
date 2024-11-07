dc2b70d6651fe25c4503edec0147a97cff0fc382
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-11-07 09:49:27 -0300

    feat(signIn/signUp pages): redirecting the user to the home page if he is already authenticated

--- src/pages/signUpPage.tsx
+++ src/pages/signUpPage.tsx
@@ -1,5 +1,12 @@
-import { SignUp } from "@components/signUp"
+import { SignUp } from "@components/signUp";
+import { useAuthContext } from "@contexts/authContext";
+import { useNavigate } from "react-router-dom";
 
 export const SignUpPage = () => {
-    return <SignUp />
-}
+  const { isAuthenticated } = useAuthContext();
+  const redirect = useNavigate();
+
+  if (isAuthenticated) return redirect("/");
+
+  return <SignUp />;
+};
--- src/pages/singInPage.tsx
+++ src/pages/singInPage.tsx
@@ -1,5 +1,12 @@
-import { SignIn } from "@components/signIn"
+import { SignIn } from "@components/signIn";
+import { useAuthContext } from "@contexts/authContext";
+import { useNavigate } from "react-router-dom";
 
 export const SignInPage = () => {
-    return <SignIn />
-}
+  const { isAuthenticated } = useAuthContext();
+  const redirect = useNavigate();
+
+  if (isAuthenticated) return redirect("/");
+
+  return <SignIn />;
+};
